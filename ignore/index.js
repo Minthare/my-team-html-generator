a4e00c764a82fdbc5f947a9d74e80699f22bdf48
MINTHARE <latrailpierre01@gmail.com>
2023-03-12 14:30:23 -0400

    updated index.js

--- ignore/index.js
+++ ignore/index.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-const Manager = require('./lib/Manager.js')
-const Engineer = require('./lib/Engineer.js')
-const Intern = require('./lib/Intern.js')
-const htmltemplate = require('./htmltemplate')
+const Manager = require('../sheet/lib/Manager.js')
+const Engineer = require('../sheet/lib/Engineer.js')
+const Intern = require('../sheet/lib/Intern.js')
+const htmltemplate = require('../sheet/src/htmltemplate')
 
 
 
